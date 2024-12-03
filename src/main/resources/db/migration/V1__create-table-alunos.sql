4cf0c51f6fd1f494fa74d7b14c88edb1d7e9cd44
Caio Henrique <caioutf@gmail.com>
2024-12-02 22:18:58 -0300

    Criação das tabelas

---
+++ src/main/resources/db/migration/V1__create-table-alunos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Alunos (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    matricula VARCHAR(20) NOT NULL UNIQUE,
+    data_nascimento DATE NOT NULL
+);
---
+++ src/main/resources/db/migration/V6__create-table-matriculas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Matriculas (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    aluno_id INT NOT NULL,
+    turma_id INT NOT NULL,
+    FOREIGN KEY (aluno_id) REFERENCES Alunos(ID) ON DELETE CASCADE,
+    FOREIGN KEY (turma_id) REFERENCES Turmas(ID) ON DELETE CASCADE
+);
